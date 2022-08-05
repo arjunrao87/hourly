7cc1d90ca172d703fcb07a953ea8046c5efc2f13
Arjun Rao <rao.arjun87@gmail.com>
2022-08-05 07:04:55 -0400

    feat: skeleton for 🧮

--- components/calculator/calculator.js
+++ components/calculator/calculator.js
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 
-export default function Calculator() {
-  const router = useRouter();
+export function diyOrNot(router) {
   const salary_currency = router.query.salary_currency;
   const salary_amount = router.query.salary_amount;
   const salary_duration = router.query.salary_duration;
@@ -11,10 +10,20 @@ export default function Calculator() {
   const task_amount = router.query.task_amount;
   const task_payment_duration = router.query.task_payment_duration;
 
+  let diy = false;
+  let message = "";
+
+  return diy, message;
+}
+
+export default function Calculator() {
+  const router = useRouter();
+  let diy,
+    message = diyOrNot(router);
   return (
     <div className="flex h-full mx-auto overflow-x-auto ">
       <div className="grid grid-cols-1 gap-6 p-4">
-        <div className="text-xl">{salary_amount}</div>
+        <div className="text-xl">{diy ? "Yes!" : "No!!!!!"}</div>
       </div>
     </div>
   );
