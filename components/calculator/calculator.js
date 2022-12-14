349eaba43e717925c082c0ff2cc6e0f761e1efd7
Arjun Rao <rao.arjun87@gmail.com>
2022-12-13 21:46:51 -0500

    fix: post -> get

--- components/calculator/calculator.js
+++ components/calculator/calculator.js
@@ -121,7 +121,7 @@ export default function Calculator() {
           </Link>
         </div>
         <div className="py-6 items-center px-8">
-          <form action="/" method="post">
+          <form action="/" method="get">
             <button
               type="submit"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
